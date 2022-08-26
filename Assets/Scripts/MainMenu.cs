58f0c7ed37a75cda6ff6972025c9278082c4ef67
Egemen Başkaya <thepoisontear@gmail.com>
2022-08-26 07:39:37 +0300

    how to play screen

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public RectTransform mainScreen;
+    public RectTransform howToPlayScreen;
+
+    private void Start()
+    {
+        howToPlayScreen.gameObject.SetActive(false);
+    }
+
     public void playGame()
     {
         SceneManager.LoadScene("game");
@@ -13,5 +21,17 @@ public class MainMenu : MonoBehaviour
     public void exitGame()
     {
         Application.Quit();
+    }
+
+    public void returnToMainMenu()
+    {
+        howToPlayScreen.gameObject.SetActive(false);
+        mainScreen.gameObject.SetActive(true);
+    }
+
+    public void howToPlay()
+    {
+        howToPlayScreen.gameObject.SetActive(true);
+        mainScreen.gameObject.SetActive(false);
     }
 }
